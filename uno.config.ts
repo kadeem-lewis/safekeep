c84a3bb098337643d10d66a2af5d02e616323aa5
kadeem-lewis <print.helloworld@genericindividual.com>
2023-06-24 06:00:51 -0400

    added preset to use for unocss

--- uno.config.ts
+++ uno.config.ts
@@ -1,6 +1,9 @@
 // uno.config.ts
 import { defineConfig } from 'unocss'
+import { presetUno } from 'unocss'
 
 export default defineConfig({
   // ...UnoCSS options
+  presets: [presetUno()],
+  shortcuts: {}
 })
