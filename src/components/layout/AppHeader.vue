9a476f989c47bcaf1c4d0199b7342da0c23e26b4
kadeem-lewis <print.helloworld@genericindividual.com>
2023-06-24 06:01:41 -0400

    began defining header

---
+++ src/components/layout/AppHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <header>
+    <Icon icon="solar:hamburger-menu-linear" :inline="true" />
+    <h1>Notes</h1>
+    <SearchBar />
+  </header>
+</template>
+
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+import SearchBar from '../SearchBar.vue'
+</script>
