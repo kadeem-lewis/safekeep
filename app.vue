f4a9a27decb37ee3def2541f8dfc297828f24869
kadeem-lewis <print.helloworld@genericindividual.com>
2023-06-24 21:27:08 -0400

    replaced layouts with app.vue since there is only a singular layout

---
+++ app.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <header class="flex justify-between items-center fixed top-0">
+      <Icon
+        name="solar:hamburger-menu-linear"
+        :inline="true"
+        class="text-2xl"
+      />
+      <h1>Notes</h1>
+      <SearchBar />
+    </header>
+    <main class="pt-16 flex">
+      <aside>
+        <menu class="list-none font-medium">
+          <li><Icon name="uil:notes" :inline="true" />Notes</li>
+          <li><Icon name="solar:trash-bin-2-linear" :inline="true" />Bin</li>
+          <li><Icon name="solar:archive-bold" :inline="true" />Archive</li>
+        </menu>
+      </aside>
+      <NuxtPage />
+    </main>
+  </div>
+</template>
+<style>
+/* @import "unocss/reset/tailwind.css"; */
+</style>
