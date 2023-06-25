ab5a9ca5ea95eb5a69a96c034b507ea39f3a2282
kadeem-lewis <print.helloworld@genericindividual.com>
2023-06-24 21:28:24 -0400

    added placeholder component to note card

--- components/NoteCard.vue
+++ components/NoteCard.vue
@@ -1,5 +1,12 @@
 <template>
-  <div></div>
+  <div class="p-4 border border-black">
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. A unde corrupti,
+      ipsa neque consectetur eius quisquam illo perspiciatis iure accusamus
+      dolor perferendis dignissimos in aperiam reiciendis minus labore ipsum
+      doloribus?
+    </p>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
