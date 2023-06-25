46bb4523ffbc0a7682536d7fcc90fd9bd6dd3f95
kadeem-lewis <print.helloworld@genericindividual.com>
2023-06-24 20:12:36 -0400

    created a default layout for app

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <header>
+    <Icon name="solar:hamburger-menu-linear" :inline="true" />
+    <h1>Notes</h1>
+    <SearchBar />
+  </header>
+  <main>
+    <SidePanel />
+    <slot />
+  </main>
+</template>
