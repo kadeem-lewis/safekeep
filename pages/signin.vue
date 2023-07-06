bcc7c134a82170ea42ab5d2d8d82f800c6e57314
kadeem-lewis <print.helloworld@genericindividual.com>
2023-07-06 00:00:56 -0400

    created a signin page for app

---
+++ pages/signin.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <button @click="signIn('github')">Sign In With Github</button>
+    <button>Sign In With Github</button>
+    <button>Sign In Anonymously</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { signIn } = useAuth();
+</script>
