6d37d5318bbece6abd94ec967c2e29cc9ea1bbd9
kadeem-lewis <print.helloworld@genericindividual.com>
2023-07-05 23:58:45 -0400

    changed from uno css to tailwind css

--- uno.config.ts
+++
@@ -1,9 +0,0 @@
-// uno.config.ts
-import { defineConfig } from 'unocss'
-import { presetUno } from 'unocss'
-
-export default defineConfig({
-  // ...UnoCSS options
-  presets: [presetUno()],
-  shortcuts: {}
-})
