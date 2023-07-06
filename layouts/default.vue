e50b295827d99f480f5772880f4b374f4d758738
kadeem-lewis <print.helloworld@genericindividual.com>
2023-07-06 01:17:22 -0400

    deleted app.vue and created two different layouts in its place

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <AppHeader />
+    <header class="flex justify-between items-center fixed top-0">
+      <Icon
+        name="solar:hamburger-menu-linear"
+        :inline="true"
+        class="text-2xl"
+      />
+    </header>
+    <main class="w-full max-w-md mx-auto p-6">
+      <slot />
+    </main>
+  </div>
+</template>
--- layouts/main.vue
+++ layouts/main.vue
@@ -1,18 +1,15 @@
 <template>
   <div>
+    <AppHeader />
     <header class="flex justify-between items-center fixed top-0">
       <Icon
         name="solar:hamburger-menu-linear"
         :inline="true"
         class="text-2xl"
       />
-      <h1>Notes</h1>
-      <SearchBar />
-      <div>
-        <button v-if="loggedIn" @click="handleSignOut">Sign Out</button>
-      </div>
     </header>
     <main class="pt-16 flex">
+      <SideBar />
       <aside>
         <menu class="list-none font-medium">
           <li><Icon name="uil:notes" :inline="true" />Notes</li>
@@ -20,18 +17,7 @@
           <li><Icon name="solar:archive-bold" :inline="true" />Archive</li>
         </menu>
       </aside>
-      <NuxtPage />
+      <slot />
     </main>
   </div>
 </template>
-<script setup lang="ts">
-const { status, signIn, signOut } = useAuth();
-const loggedIn = computed(() => status.value === "authenticated");
-
-async function handleSignOut() {
-  await signOut();
-}
-</script>
-<style>
-/* @import "unocss/reset/tailwind.css"; */
-</style>
