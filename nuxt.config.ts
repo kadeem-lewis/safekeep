9386163276079921abbf629a2c8167e8b6b0a847
kadeem-lewis <email@kadeemlewis.com>
2024-02-23 17:48:54 -0500

    removed auth modules from nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "nuxt-icon", "@sidebase/nuxt-auth"],
-  auth: {
-    provider: {
-      type: "authjs",
-    },
-  },
+  modules: ["@nuxtjs/tailwindcss", "nuxt-icon"],
   runtimeConfig: {
     GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
     public: {
       GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
-    },
-  },
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
     },
   },
 });
