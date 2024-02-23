238aa274e696644157e32e71218a47d8c2838523
kadeem-lewis <email@kadeemlewis.com>
2024-02-23 17:49:58 -0500

    deleted server directory content

--- server/api/notes/index.get.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-export const prisma = new PrismaClient();
-
-export default defineEventHandler(async (event) => {
-  const data = await prisma.note.findMany();
-  return data;
-});
