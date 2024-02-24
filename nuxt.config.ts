6e980275c50a372ee6e928fc56c373d0ba9dac6d
kadeem-lewis <email@kadeemlewis.com>
2024-02-23 23:34:14 -0500

    added firebase variables to vuefire config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "nuxt-icon"],
+  modules: ["@nuxt/ui", "@nuxtjs/eslint-module","nuxt-vuefire"],
   runtimeConfig: {
     GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
     public: {
       GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
     },
   },
+  vuefire:{
+    auth:{
+      enabled: true,
+    },
+    config:{
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
+      measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    }
+  }  
 });
