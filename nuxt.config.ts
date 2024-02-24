6da56fa6fbe4e02422cd3bd4ffa005018dc88a5a
kadeem-lewis <email@kadeemlewis.com>
2024-02-24 00:29:56 -0500

    deleted measurement id since it doesn't exist

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,7 +19,6 @@ export default defineNuxtConfig({
       storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
       messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
       appId: process.env.FIREBASE_APP_ID,
-      measurementId: process.env.FIREBASE_MEASUREMENT_ID
     }
   }  
 });
