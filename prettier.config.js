325e1d1252a9af48109784449b646b61f163cbfc
kadeem-lewis <email@kadeemlewis.com>
2024-02-23 23:33:32 -0500

    configured prettier and prettier plugin tailwindcss

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+// prettier.config.js
+
+/** @type {import("prettier").Config} */
+const config = {
+  plugins: ["prettier-plugin-tailwindcss"],
+  tailwindConfig: "./tailwind.config.ts",
+};
+
+export default config;
